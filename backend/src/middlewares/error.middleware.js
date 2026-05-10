1a6155244b9dd85dfebd40618d74ffefa6126e83
Yashmandloi1411 <yashmandloi1411@gmail.com>
2026-05-10 17:45:40 +0530

    fixing all the issue realted to prod

--- backend/src/middlewares/error.middleware.js
+++ backend/src/middlewares/error.middleware.js
@@ -2,7 +2,7 @@ function handleError(err, req, res, next) {
   const response = {
     message: err.message || "Internal Server Error",
   };
-  if (process.env.NODE_ENVIRONMENT === "development") {
+  if (process.env.NODE_ENV === "development") {
     response.stack = err.stack;
   }
   res.status(err.status || 500).json(response);
